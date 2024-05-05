8f6ecc2fb1c34947672b5c7a1b101e177b4c39d0
sage0711 <pamewabnnx@gmail.com>
2024-05-05 04:26:16 -0400

    fix: lazy loading

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,23 +1,27 @@
-import Home from "../../components/Pages/Home";
-import Header from "../../components/Pages/Header";
-import Benifit from "../../components/Pages/Benifit";
-import About from "../../components/Pages/About";
-import Market from "../../components/Pages/Market";
-import Tokenomics from "../../components/Pages/Tokenomics";
-import Footer from "../../components/Pages/Footer";
+import { Suspense, lazy } from "react";
 import { Box } from "@mui/joy";
+
+const Home = lazy(() => import("../../components/Pages/Home"));
+const Header = lazy(() => import("../../components/Pages/Header"));
+const Benifit = lazy(() => import("../../components/Pages/Benifit"));
+const About = lazy(() => import("../../components/Pages/About"));
+const Market = lazy(() => import("../../components/Pages/Market"));
+const Tokenomics = lazy(() => import("../../components/Pages/Tokenomics"));
+const Footer = lazy(() => import("../../components/Pages/Footer"));
 
 const Main = () => {
   return (
-    <Box>
-      <Header />
-      <Home />
-      <About />
-      <Benifit />
-      <Market />
-      <Tokenomics />
-      <Footer />
-    </Box>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Box>
+        <Header />
+        <Home />
+        <About />
+        <Benifit />
+        <Market />
+        <Tokenomics />
+        <Footer />
+      </Box>
+    </Suspense>
   );
 };
 
